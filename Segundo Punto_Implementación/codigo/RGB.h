b77be0d46ceb0841e7a0749b565996cbb1d82cec
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-09-24 23:40:19 -0500

    Implementación código para Tinkercad + nuevas ideas

--- Segundo Punto_Implementación/codigo/RGB.h
+++ Segundo Punto_Implementación/codigo/RGB.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <QImage>
 #include <list>
+#include <fstream>
 
 using namespace std;
 
@@ -11,21 +12,14 @@ class RGB
 
 public:
     RGB();
-    void color_pixel(int red, int green, int blue);
-    void ubicacion_archivo();
     void capturaRGB(QImage imagen);
-
-
+    void escribir_Archivo(int *datos);
 
 private:
-    //map<char, vector<int> > MapRGB;
-    //vector<int> VectorRGB={0};
     int Red, Blue,Green;
     list<list<int>> RGB_lista[3];
     list<int> lista;
     string filename;
-    //QImage imagen;
-
 };
 
 
